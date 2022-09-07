83dc40107d711edd4537f0bed05ab46231040d3f
brady293 <brady0330y@gmail.com>
2022-09-07 19:31:46 +0800

    瀏覽器同步設定

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+// import "bootstrap";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
